a81cd200426ef3ef02c04f038bdbd47c83d0b954
thijmenblokland <thijmenblokland@hotmail.com>
2023-08-19 13:28:15 +0200

    1.0.0

---
+++ SCP079.AntiTeslaTrigger/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace SCP079.AntiTeslaTrigger
+{
+    public class Config : IConfig
+    {
+        [Description("Whether or not the plugin is enabled.")]
+        [DefaultValue(true)]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Whether to show debug messages.")]
+        [DefaultValue(false)]
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ SCP079.AntiTeslaTrigger/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Exiled.API.Features;
+using Server = Exiled.Events.Handlers.Server;
+using Tesla = Exiled.API.Features.TeslaGate;
+
+namespace SCP079.AntiTeslaTrigger
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Scp079 Anti-Tesla-Trigger";
+        public override string Author => "Hexa";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(7, 2, 0);
+
+        public override void OnEnabled()
+        {
+            Server.RoundStarted += OnRoundStart;
+        }
+
+        public override void OnDisabled()
+        {
+            Server.RoundStarted -= OnRoundStart;
+        }
+
+        private void OnRoundStart()
+        {
+            if (Config.IsEnabled)
+            {
+                Tesla.IgnoredRoles.Add(PlayerRoles.RoleTypeId.Scp079);
+
+                if (Config.Debug)
+                {
+                    Log.Debug("Disabled tesla triggering for SCP-079");
+                }
+            }
+        }
+        }
+    }
